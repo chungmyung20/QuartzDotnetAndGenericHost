a5547af741b74439ad9206a3f9273abc51394cb3
이상준 <christian289@naver.com>
2021-12-14 19:37:20 +0900

    커밋

---
+++ QuartzDotnetAndGenericHost/Services/MyService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace QuartzDotnetAndGenericHost.Services
+{
+    public class MyService
+    {
+        public MyService()
+        {
+
+        }
+
+        public void MyVoidMethod()
+        {
+            Console.WriteLine("Void 메서드 실행!!");
+        }
+
+        public int MyIntMethod()
+        {
+            Console.WriteLine("Int 메서드 실행!!");
+
+            return default;
+        }
+    }
+}
